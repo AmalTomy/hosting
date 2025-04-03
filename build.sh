15ecfef804c4c841b394f37237dabb66710b2705
AmalTomy <77141896+AmalTomy@users.noreply.github.com>
2025-04-03 13:25:54 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -13,3 +13,9 @@ python manage.py collectstatic --no-input
 
 # Apply migrations
 python manage.py migrate
+
+# Create default site if it doesn't exist
+python manage.py shell -c "
+from django.contrib.sites.models import Site
+Site.objects.get_or_create(id=1, defaults={'domain': 'hosting-kq0v.onrender.com', 'name': 'LogU Travel Agency'})
+"
